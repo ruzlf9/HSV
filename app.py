3cf0a742a941d3412a4b0c302e634e68a3c01d14
ruzlf9 <150386197+ruzlf9@users.noreply.github.com>
2023-11-15 16:57:42 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -28,6 +28,12 @@ TEAMS = [
 @app.route("/")
 def hello_world():
   return render_template("home.html", teams=TEAMS)
+
+
+@app.route("/api/teams")
+def team_jobs():
+  return jsonify(TEAMS)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
