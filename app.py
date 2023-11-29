0d98172860d39d1837f71d61d1fc26a2b6c3f8ea
ruzlf9 <150386197+ruzlf9@users.noreply.github.com>
2023-11-29 11:42:45 +0000

    Created teamsites

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 from datetime import timedelta
+import datetime
 
 
 app = Flask(__name__)
@@ -11,23 +12,51 @@ TEAMS = [
     "id": "U19",
     "name": "U19",
     "coaches" : ["Marvin","Justus"],
-    "players": ["A","B","C"]
+    "players": [
+      {
+        "name": "Justin_Test",
+        "birthdate": datetime.datetime(2020, 5, 17).strftime("%x")
+      },
+      {
+        "name": "Tim_Test",
+        "birthdate": datetime.datetime(2020, 8, 12).strftime("%x")
+      }
+    ]
   },
 
   {
     "id": "U17",
     "name": "U17",
     "coaches" : ["Holger","Rudi"],
-    "players": ["D","E","F"]
+    "players": [
+      {
+        "name": "Ali_Test",
+        "birthdate": datetime.datetime(2021, 2, 2).strftime("%x")
+      },
+      {
+        "name": "Noah_Test",
+        "birthdate": datetime.datetime(2021, 4, 19).strftime("%x")
+      }
+    ]
   },
 
   {
     "id": "U16",
     "name": "U16",
     "coaches" : ["Lukas","Niklas"],
-    "players": ["G","H","I"]
+    "players": [
+      {
+        "name": "Younes_Test",
+        "birthdate": datetime.datetime(2022, 2, 19).strftime("%x")
+      },
+      {
+        "name": "Joshua_Test",
+        "birthdate": datetime.datetime(2022, 8, 12).strftime("%x")
+      }
+    ]
   }
 ]
+ids = {"U19":0, "U17":1, "U16":2}
 
 @app.route("/")
 def start():
@@ -58,9 +87,21 @@ def authenticate_user(email, password):
   return email == 'Test@Test.com' and password == 'password'
 
 
+@app.route("/<team_id>")
+def team(team_id):
+  if "user" in session:
+    print(TEAMS[ids[team_id]]["name"])
+    return render_template("team.html", team=TEAMS[ids[team_id]], user=session["user"])
+  else:
+    return render_template('home_lock.html', error='Zugangsdaten falsch')
+
+
 @app.route("/api/teams")
 def team_jobs():
-  return jsonify(TEAMS)
+  if "user" in session:
+    return jsonify(TEAMS)
+  else:
+    return render_template('home_lock.html', error='Zugangsdaten falsch')
 
 
 if __name__ == "__main__":
