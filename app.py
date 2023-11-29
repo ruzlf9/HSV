4a408c5c6bfa8f194aa22f7b8adb5475096325c3
ruzlf9 <150386197+ruzlf9@users.noreply.github.com>
2023-11-29 15:18:17 +0000

    added external players

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for, s
 from datetime import timedelta
 import datetime
 
+from flask.typing import TemplateTestCallable
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
-app.permanent_session_lifetime = timedelta(seconds=10)
+#app.permanent_session_lifetime = timedelta(seconds=10)
 
 TEAMS = [
   {
@@ -14,12 +16,14 @@ TEAMS = [
     "coaches" : ["Marvin","Justus"],
     "players": [
       {
-        "name": "Justin_Test",
-        "birthdate": datetime.datetime(2020, 5, 17).strftime("%x")
+        "Vorname": "Justin",
+        "Nachname": "Test",
+        "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
       },
       {
-        "name": "Tim_Test",
-        "birthdate": datetime.datetime(2020, 8, 12).strftime("%x")
+        "Vorname": "Justin",
+        "Nachname": "Test",
+        "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
       }
     ]
   },
@@ -30,12 +34,14 @@ TEAMS = [
     "coaches" : ["Holger","Rudi"],
     "players": [
       {
-        "name": "Ali_Test",
-        "birthdate": datetime.datetime(2021, 2, 2).strftime("%x")
+        "Vorname": "Justin",
+        "Nachname": "Test",
+        "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
       },
       {
-        "name": "Noah_Test",
-        "birthdate": datetime.datetime(2021, 4, 19).strftime("%x")
+        "Vorname": "Justin",
+        "Nachname": "Test",
+        "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
       }
     ]
   },
@@ -46,17 +52,32 @@ TEAMS = [
     "coaches" : ["Lukas","Niklas"],
     "players": [
       {
-        "name": "Younes_Test",
-        "birthdate": datetime.datetime(2022, 2, 19).strftime("%x")
+        "Vorname": "Justin",
+        "Nachname": "Test",
+        "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
       },
       {
-        "name": "Joshua_Test",
-        "birthdate": datetime.datetime(2022, 8, 12).strftime("%x")
+        "Vorname": "Justin",
+        "Nachname": "Test",
+        "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
       }
     ]
   }
 ]
 ids = {"U19":0, "U17":1, "U16":2}
+
+EXTERNAL_PLAYERS = [
+  {
+    "Vorname": "Test",
+    "Nachname": "Nachname",
+    "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
+  },
+  {
+    "Vorname": "Test2",
+    "Nachname": "Nachname2",
+    "Geburtsdatum": datetime.datetime(2020, 5, 17).strftime("%x")
+  }
+]
 
 @app.route("/")
 def start():
@@ -74,8 +95,6 @@ def home():
 def login():
   email = request.form.get('email')
   password = request.form.get('password')
-  print(email)
-  print(password)
   
   if authenticate_user(email, password):
     session["user"] = email
@@ -90,8 +109,15 @@ def authenticate_user(email, password):
 @app.route("/<team_id>")
 def team(team_id):
   if "user" in session:
-    print(TEAMS[ids[team_id]]["name"])
-    return render_template("team.html", team=TEAMS[ids[team_id]], user=session["user"])
+    return render_template("team.html", team=TEAMS[ids[team_id]], user=session["user"], teams=TEAMS)
+  else:
+    return render_template('home_lock.html', error='Zugangsdaten falsch')
+
+
+@app.route("/external")
+def external():
+  if "user" in session:
+    return render_template("external.html", user=session["user"], players=EXTERNAL_PLAYERS, teams=TEAMS)
   else:
     return render_template('home_lock.html', error='Zugangsdaten falsch')
 
